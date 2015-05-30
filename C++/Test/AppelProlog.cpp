de5cf642d62d65947916cb243805bb2eb784d8fc
Dulieu Romain <dulieu.r@gmail.com>
2015-05-30 14:46:48 +0200

    add AppelProlog.cpp

---
+++ C++/Test/AppelProlog.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdlio>
+
+public void main(){
+
+	PrologInterface m_PrologInterface;
+	m_PrologInterface.init( “prgp.exe” );
+
+	if (m_PrologInterface.start( "startThinking", 1 )) // Le prédicat startThinking ayant un argument va être testé
+	{
+		if (m_PrologInterface.call()) // teste le prédicat Prolog
+		{
+			cout << "Thinking initialized!" << endl;
+		}
+	else
+	{
+		cout << "Thinking NOT initialized!" << endl;
+	}
+		m_PrologInterface.finish(); // indique que le prédicat a fini d'être testé
+	}
+}
